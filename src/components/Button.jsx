e9403a193679a8fd248170aa187fc56726a183a1
haileiyesus-29 <haileiyesus29@gmail.com>
2024-06-04 01:54:17 +0300

    new branch to serve the pages

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-function Button({ label }) {
-   return (
-      <button className='hover:bg-gray-200/90 dark:bg-gray-200 px-3 py-2 rounded-[4px] font-semibold dark:text-gray-900 transition-all'>
-         {label}
-      </button>
-   )
-}
-export default Button
