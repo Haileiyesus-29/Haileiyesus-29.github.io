ea59081a0347719d0513606eaf09fdc511310ebe
haileiyesus-29 <haileiyesus29@gmail.com>
2024-07-11 17:04:23 +0300

    change styles

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const toggle = document.querySelector('#toggle')
+
 const contactForm = document.querySelector('#contact_form')
 const nameInput = document.querySelector('#name')
 const emailInput = document.querySelector('#email')
@@ -9,6 +11,29 @@ const messageErrEl = document.querySelector('#messageErr')
 
 const emailErr = false
 const messageErr = false
+
+// Check for saved 'darkMode' in localStorage
+localTheme = localStorage.getItem('theme')
+   ? localStorage.getItem('theme')
+   : 'light'
+if (localTheme === 'dark') {
+   document.documentElement.classList.replace('light', 'dark')
+   toggle.checked = true
+} else {
+   document.documentElement.classList.replace('dark', 'light')
+   toggle.checked = false
+}
+
+// Switch theme
+toggle.addEventListener('change', e => {
+   if (e.target.checked) {
+      document.documentElement.classList.replace('light', 'dark')
+      localStorage.setItem('theme', 'dark')
+   } else {
+      document.documentElement.classList.replace('dark', 'light')
+      localStorage.setItem('theme', 'light')
+   }
+})
 
 contactForm.addEventListener('submit', e => {
    e.preventDefault()
