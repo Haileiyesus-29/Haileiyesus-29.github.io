c0e8f90494513b392cab429a6a9a74708076d562
haileiyesus-29 <haileiyesus29@gmail.com>
2024-07-11 17:05:19 +0300

    add new logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   darkMode: 'class',
    content: ['./src/**/*.{html,js}'],
    theme: {
       extend: {
@@ -9,6 +10,14 @@ module.exports = {
          },
          fontFamily: {
             body: ['Geist', 'GeistMono'],
+         },
+         backgroundSize: {
+            repeated: '22px 22px',
+         },
+         backgroundImage: {
+            lightPattern: 'radial-gradient(#a2a2a2 1.1px, #f3f4f6 1.1px)',
+            darkPattern:
+               'radial-gradient(#3b3b3b 1.1px, transparent 1.1px), radial-gradient(#3b3b3b 1.1px, #121224 1.1px)',
          },
       },
    },
